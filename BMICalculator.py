4146c2d9877b6f9085dd60910516a4508a88028a
beattytyler <166061554+beattytyler@users.noreply.github.com>
2024-06-10 06:27:19 -0400

    BMICalculator.py

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,9 +1,1 @@
-# take user input for measurements
-height = int(input('What is your height in inches? '))
-weight = int(input('What is your weight in pounds? '))
 
-# formula for BMI
-bmi = (weight / (height ** 2)) * 703
-
-# print output
-print(bmi)
